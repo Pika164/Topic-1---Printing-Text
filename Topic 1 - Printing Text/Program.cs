7d7021e946eb46cbcdbde491a230134bf33b2c0f
339742660 <339742660@2260I-L29-020>
2025-02-04 14:01:12 -0500

    Add project files.

---
+++ Topic 1 - Printing Text/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Topic_1___Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Linkin Krysa
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This will print on a new line");
+
+            Console.WriteLine();
+
+            Console.Write("Hello, World!");
+            Console.Write(" This will print on the same line");
+
+
+            Console.WriteLine("hello");
+        }
+    }
+}
